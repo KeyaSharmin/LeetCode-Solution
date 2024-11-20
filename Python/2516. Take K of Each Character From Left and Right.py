132d5328b3e00bec38ea0eabad217801bc03ad6a
Sharmin Akter Keya <keyasharmin100@gmail.com>
2024-11-20 08:31:55 -0500

    Solved in November

---
+++ Python/2516. Take K of Each Character From Left and Right.py
@@ -0,0 +1,20 @@
+class Solution:
+    def takeCharacters(self, s: str, k: int) -> int:
+        freqs = [0] * 3
+        n = len(s)
+        
+        for c in s:
+            freqs[ord(c) - ord('a')] += 1
+        
+        i = 0
+        j = 0
+        if freqs[0] < k or freqs[1] < k or freqs[2] < k:
+            return -1
+        
+        for j in range(n):
+            freqs[ord(s[j]) - ord('a')] -= 1
+            
+            if freqs[0] < k or freqs[1] < k or freqs[2] < k:
+                freqs[ord(s[i]) - ord('a')] += 1
+                i += 1
+        return n - (j - i + 1)
