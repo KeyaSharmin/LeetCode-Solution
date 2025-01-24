36a1a226e3f77e2daf4df939c77c969abf1b4f55
Sharmin Akter Keya <keyasharmin100@gmail.com>
2025-01-24 08:34:35 -0500

    Solved in January

--- Python/2017. Grid Game.py
+++ Python/2017. Grid Game.py
@@ -1,12 +1,8 @@
 class Solution:
-    def gridGame(self, grid):
-        top = sum(grid[0])
-        bottom = 0
-        res = float('inf')
-        
-        for i in range(len(grid[0])):
-            top -= grid[0][i]
-            res = min(res, max(top, bottom))
-            bottom += grid[1][i]
-        
-        return res
+    def gridGame(self, grid: List[List[int]]) -> int:
+        one, two = sum(grid[0]), 0
+        for x, y in zip(*grid):
+            one -= x
+            two += y
+            if two > one:
+                return max(one, two - y)
