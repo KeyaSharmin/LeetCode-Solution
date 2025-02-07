b5e7b60cd58660a339a0c1b7e23ebece81a01ab9
Sharmin Akter Keya <keyasharmin100@gmail.com>
2025-02-07 08:27:24 -0500

    Solved in February

---
+++ Python/3160. Find the Number of Distinct Colors Among the Balls.py
@@ -0,0 +1,16 @@
+class Solution:
+    def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
+        balls = {}
+        colours = {}
+        res = []
+        for b, c in queries:
+            if b in balls:
+                if colours[balls[b]] == 1:
+                    del colours[balls[b]]
+                else:
+                    colours[balls[b]] -= 1
+            colours[c] = colours.get(c, 0) + 1
+            balls[b] = c
+            res.append(len(colours))
+        
+        return res
