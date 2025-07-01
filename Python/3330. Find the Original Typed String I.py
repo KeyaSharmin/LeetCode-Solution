a57686ca3384cd3f2c3ac954a883d447685761f0
Sharmin Akter Keya <keyasharmin100@gmail.com>
2025-07-01 08:25:07 -0400

    Solved in July

---
+++ Python/3330. Find the Original Typed String I.py
@@ -0,0 +1,7 @@
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+        p, cnt, ans = "", 0, 1
+        for w in word:
+            if w == p: cnt += 1
+            else: ans += cnt; p, cnt = w, 0
+        return ans + cnt
