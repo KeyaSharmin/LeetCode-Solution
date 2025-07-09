343657f6a7f62160a763e19d02071c5275a0c834
Sharmin Akter Keya <keyasharmin100@gmail.com>
2025-07-09 08:35:39 -0400

    Solved in July

---
+++ Python/3439. Reschedule Meetings for Maximum Free Time I.py
@@ -0,0 +1,4 @@
+class Solution:
+    def maxFreeTime(self, t: int, k: int, s: List[int], e: List[int]) -> int:
+        q = [*map(sub,s+[t],[0]+e)]
+        return max(accumulate(map(sub,q[k+1:],q),initial=sum(q[:k+1])))
