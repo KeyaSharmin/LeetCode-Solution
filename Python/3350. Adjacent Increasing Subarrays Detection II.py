8410926a80aa0f559ef6ba70402c7146f1ecf275
Sharmin Akter Keya <keyasharmin100@gmail.com>
2025-10-15 08:32:36 -0400

    Solved in October

---
+++ Python/3350. Adjacent Increasing Subarrays Detection II.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxIncreasingSubarrays(self, nums: List[int]) -> int:
+        prevl = 1
+        l = 1
+        longest = 1
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i-1]:
+                longest = max(longest, min(l, prevl), l // 2)
+                prevl = l
+                l = 1
+            else:
+                l += 1
+        
+        longest = max(longest, min(l, prevl), l // 2)
+        return longest
